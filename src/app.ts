ec3c17f89d2b266d9ac8e7e52b3d13063a867756
msscsh <itsbruno42@gmail.com>
2023-05-18 10:33:13 -0300

    adding brawlstars-api-sdk

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import BrawlStarsAPICaller from "brawlstars-api-sdk";
+
+const client = new BrawlStarsAPICaller({
+    apiKey: "AAAA"
+});
+
+client.getPlayer('#2QPVJ099C').then((player) => {
+    console.log(player);
+});
