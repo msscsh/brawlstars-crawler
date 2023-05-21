22659b78240cc41d060003e9bec3978df4c6077f
msscsh <itsbruno42@gmail.com>
2023-05-21 02:08:14 -0300

    Fixing inconsistencies in the display of options

--- src/competition/fandom-quiz/app.ts
+++ src/competition/fandom-quiz/app.ts
@@ -1,13 +1,25 @@
+import { log } from 'console';
 import fs from 'fs';
 import path from 'path';
 
+// interface Brawler {
+//   nome: string;
+//   vida: number;
+//   ataque: number;
+//   velocidade: string;
+//   super: string;
+// }
+
 interface Brawler {
-  nome: string;
-  vida: number;
-  ataque: number;
-  velocidade: string;
-  super: string;
-}
+    'nome': string;
+    'vida': number;
+    'Dano máximo por munição': number;
+    'Raridade': string;
+    'Range': string;
+    'Velocidade de carregamento': string;
+    'Velocidade de movimento': string;
+    'Tipo': string;
+  }
 
 interface Question {
   type: string;
@@ -17,40 +29,20 @@ interface Question {
   attribute: string
 }
 
-//Leitura do arquivo
-const pasta = '';
-const nomeArquivo = 'informacoes.json';
-const caminhoArquivo = path.join(__dirname, pasta, nomeArquivo);
-
-//conversão para objeto
-const rawData = fs.readFileSync(caminhoArquivo, 'utf8');
-const brawlers: Brawler[] = JSON.parse(rawData).brawlers;
-
-//Lista de valores para super
-const valoresAtributosSuper = [];
-for (const brawler of brawlers) {
-  const valor = brawler['super'];
-  valoresAtributosSuper.push(valor);
-}
-
-//lista de atributos válidos para perguntas
-const brawlersValidAttributesList = getBrawlerAttributes(brawlers[0])
-                    .filter(item => (item !== 'nome') && (item !== 'super') );
-
-const brawlersDirectValuesList = getBrawlerAttributes(brawlers[0])
-                    .filter(item => item === 'super');
-
-
 // Variáveis globais para controlar as combinações já utilizadas
 let usedBrawlerGroups: Brawler[] = [];
 let usedQuestionTypes: string[] = [];
 let usedAttributes: string[] = [];
-
 const questions: Question[] = [];
+
+let brawlers: Brawler[] = [];
+let valoresAtributosSuper = [];
+let brawlersValidAttributesList = [];
+let brawlersDirectValuesList = [];
+
 function main() {
-    const numQuestions = 5; // Número de perguntas desejado
+    const numQuestions = 10; // Número de perguntas desejado
     
-  
     for (let i = 0; i < numQuestions; i++) {
       const question = generateUniqueQuestion();
       questions.push(question);
@@ -58,8 +50,7 @@ function main() {
   
     // Iterar sobre a lista de perguntas
     for (const question of questions) {
-      console.log('Tipo de Pergunta:', question.type);
-      console.log('Atributo alvo da pergunta:', question.attribute);
+      console.log('----------------------');
       console.log('Pergunta:', question.question);
       console.log('Opções:', question.options);
       console.log('Resposta:', question.answer);
@@ -67,6 +58,8 @@ function main() {
     }
 }
 
+createBrawlersFromFile();
+inicializarGlobais();
 main();
 
 function generateUniqueQuestion(): Question {
@@ -84,15 +77,8 @@ function generateUniqueQuestion(): Question {
   
 function isCombinationUsed(selectedBrawler: Brawler, type: string, attribute: string): boolean {
     const hasUsedBrawlerGroup = usedBrawlerGroups.includes(selectedBrawler);  
-    const hasUsedQuestionType = usedQuestionTypes.includes(type);
-    const hasUsedAttribute = usedAttributes.includes(attribute);
-
-    console.log(selectedBrawler + ' = ' + hasUsedBrawlerGroup );
-    console.log(type + ' = ' + hasUsedQuestionType);
-    console.log(attribute + ' = ' + hasUsedAttribute );
-
-    console.log(questions)
-
+    // const hasUsedQuestionType = usedQuestionTypes.includes(type);
+    // const hasUsedAttribute = usedAttributes.includes(attribute);
     return hasUsedBrawlerGroup;
 }
   
@@ -124,7 +110,7 @@ function generateAttributeQuestion(attribute): Question {
 
   const question = `Qual brawler possui maior ${attribute}?`;
   const answer = selectedBrawlers.reduce((a, b) => (a[attribute] > b[attribute] ? a : b)).nome;
-  const options = gerarOpcoesComValoresDosAtributosDosBrawlers(selectedBrawlers, 'nome');
+  const options = gerarOpcoesComValoresDosAtributosDosBrawlers(selectedBrawlers, 'nome', answer);
 
   return {
     type: 'Perguntas de cruzamento de informação de atributos do brawler',
@@ -140,11 +126,12 @@ function getBrawlerAttributes(brawler: Brawler): string[] {
 }
 
 function generateDirectInfoQuestion(attribute): Question {
+
   const selectedBrawlers = gerarListaDeBrawlersComBrawlerAlvo();
   const brawler = selectedBrawlers[0];
-  const question = `Qual é a habilidade Super do brawler ${brawler.nome}?`;
-  const options = gerarOpcoesComValoresDosAtributosDosBrawlers(selectedBrawlers, attribute);
-  const answer = brawler.super;
+  const question = `Qual é o/a ${attribute} do brawler ${brawler.nome}?`;
+  const answer = brawler[attribute];
+  const options = gerarOpcoesComValoresDosAtributosDosBrawlers(undefined, attribute, answer);
 
   return {
     type: 'Perguntas de informação direta do brawler',
@@ -163,8 +150,26 @@ function shuffle(array: any[]) {
   return array;
 }
 
-function gerarOpcoesComValoresDosAtributosDosBrawlers(brawlersSelecionados, atributo) {
-    return brawlersSelecionados.map(brawler => brawler[atributo]);
+function gerarOpcoesComValoresDosAtributosDosBrawlers(brawlersSelecionados, attribute, answer) {
+    
+    if(brawlersSelecionados) {
+        return brawlersSelecionados.map(brawler => brawler[attribute]);
+    }
+
+    const distinctValues = new Set<string>();
+    distinctValues.add(answer);
+
+    while (distinctValues.size < 4) {
+      const randomBrawler = brawlers[Math.floor(Math.random() * brawlers.length)];
+      const attributeValue = randomBrawler[attribute];
+  
+      if (!distinctValues.has(attributeValue)) {
+        distinctValues.add(attributeValue);
+      }
+    }
+  
+    return Array.from(distinctValues);
+
 }
 
 function gerarListaDeBrawlersComBrawlerAlvo() {
@@ -184,3 +189,72 @@ function gerarListaDeBrawlersComBrawlerAlvo() {
 
     return shuffle(brawlersSelecionados);
 }
+
+
+function createBrawlersFromFile(): void {
+  const brawlers: Brawler[] = [];
+  
+
+//Leitura do arquivo
+const pasta1 = '';
+const nomeArquivo1 = 'dados_brawlers.txt';
+const caminhoArquivo1 = path.join(__dirname, pasta1, nomeArquivo1);
+  const lines = fs.readFileSync(caminhoArquivo1, 'utf8').split('\n');
+  
+  for (const line of lines) {
+    const values = line.trim().split('\t');
+
+    if(values[0] === 'Nome') {
+        continue;
+    }
+
+    if (values.length === 8) {
+      let brawler2 = {
+        'nome': values[0],
+        'vida': parseInt(values[1], 10),
+        'Dano máximo por munição': parseInt(values[2], 10),
+        'Raridade': values[3],
+        'Range': values[4],
+        'Velocidade de carregamento': values[5],
+        'Velocidade de movimento': values[6],
+        'Tipo': values[7],
+      };
+      
+      brawlers.push(brawler2);
+    }
+  }
+  
+  //Leitura do arquivo
+  const pasta = '';
+  const nomeArquivo = 'informacoes.json';
+  const caminhoArquivo = path.join(__dirname, pasta, nomeArquivo);
+  fs.writeFileSync(caminhoArquivo, JSON.stringify(brawlers, null, 2));
+}
+
+function inicializarGlobais() {    
+
+    //Leitura do arquivo
+    const pasta = '';
+    const nomeArquivo = 'informacoes.json';
+    const caminhoArquivo = path.join(__dirname, pasta, nomeArquivo);
+
+    //conversão para objeto
+    const rawData = fs.readFileSync(caminhoArquivo, 'utf8');
+    brawlers = JSON.parse(rawData);
+
+    //Lista de valores para super
+    valoresAtributosSuper = [];
+    for (const brawler of brawlers) {
+        const valor = brawler['super'];
+        valoresAtributosSuper.push(valor);
+    }
+
+    //lista de atributos válidos para perguntas
+    brawlersValidAttributesList = getBrawlerAttributes(brawlers[0])
+                        .filter(item => (item === 'vida') || (item === 'Dano máximo por munição') );
+
+    brawlersDirectValuesList = getBrawlerAttributes(brawlers[0])
+                        .filter(item => (item === 'Velocidade de movimento') || (item === 'Velocidade de carregamento') || (item === 'Range') || (item === 'Tipo') || (item === 'Raridade'));
+
+}
+
