ee60a38eb692a1b68b1b23b9e35570659869d19e
msscsh <itsbruno42@gmail.com>
2023-07-11 13:57:51 -0300

    adding integration component with googlespreadsheet

---
+++ src/commons/functions/googlespreadsheet-operations.ts
@@ -0,0 +1,52 @@
+import { PlayersData } from 'src/commons/types/players-data';
+import { readJSONInFile } from '../../commons/functions/files';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const spreadsheetId = process.env.SPREADSHEETID || "10bxlToXdOLsyl2wER2W1TcA9auYmPPqg00RPhIysiZk";
+console.log(`The spreadsheetId ${spreadsheetId}`);
+const spreadsheetTargetPlayerData = process.env.SPREADSHEETTAB+'!'+process.env.PLAYERCELLS;
+console.log(`The players cells location  ${spreadsheetTargetPlayerData}`);
+
+export function getPlayers(): any {
+    const data: PlayersData = readJSONInFile('club_data.json');
+    return data.members;
+}
+
+export async function addLinesInSheet(googleSheets, auth, lines) {
+  await googleSheets.spreadsheets.values.append({
+    auth,
+    spreadsheetId,
+    range: spreadsheetTargetPlayerData,
+    valueInputOption: "USER_ENTERED",
+    resource: {
+      values: lines,
+    },
+  });
+}
+
+export async function getDataInSheetArea(googleSheets, auth) {
+  const getRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: spreadsheetTargetPlayerData,
+  });
+  return getRows;
+}
+
+export async function clearPlayersLines(googleSheets, auth) {
+  await googleSheets.spreadsheets.values.clear(
+    {
+      auth,
+      spreadsheetId,
+      range: process.env.SPREADSHEETTAB+'!'+'A2:C',
+    },
+    (err, response) => {
+      if (err) {
+        console.error('Something is wrong i can feel it:', err);
+        return;
+      }
+      console.log("It's all gone!");
+    });
+}
