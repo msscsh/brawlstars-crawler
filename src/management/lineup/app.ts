19504966066d67e31a99d0f8001315d318253266
msscsh <itsbruno42@gmail.com>
2023-07-11 13:56:26 -0300

    removing need for configuration feeder app and lineup app for lineup presentation

--- src/management/lineup/app.ts
+++
@@ -1,7 +0,0 @@
-import { PlayersData } from 'src/commons/types/players-data';
-import { readJSONInFile } from '../../commons/functions/files';
-
-export function getPlayers(): any {
-    const data: PlayersData = readJSONInFile('club_data.json');
-    return data.players;
-}
