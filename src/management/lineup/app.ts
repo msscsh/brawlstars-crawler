fcca27b7b0119433ec1c557655dddb5038b0051d
msscsh <itsbruno42@gmail.com>
2023-07-10 21:35:12 -0300

    changing app lineup

--- src/management/lineup/app.ts
+++ src/management/lineup/app.ts
@@ -1,16 +1,7 @@
 import { PlayersData } from 'src/commons/types/players-data';
 import { readJSONInFile } from '../../commons/functions/files';
 
-function main() {
-
+export function getPlayers(): any {
     const data: PlayersData = readJSONInFile('club_data.json');
-
-    for (const player of data.players) {
-      console.log('----------------------');
-      console.log('Name:', player.name);
-      console.log('Trophies:', player.trophies);
-      console.log('----------------------');
-    }
+    return data.players;
 }
-
-main();
