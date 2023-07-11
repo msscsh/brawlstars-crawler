c276cc0ff5e777118353ae84dea551b37ec3926c
msscsh <itsbruno42@gmail.com>
2023-07-11 13:58:33 -0300

    adding server for creating express endpoints

--- src/server.js
+++ src/server.js
@@ -2,50 +2,60 @@ const { log } = require("console");
 const express = require("express");
 const { google } = require("googleapis");
 
-const { getPlayers } = require('../dist/management/lineup/app');
+const { transformTwoDimensionalArrayToListOfPlayers, valueAlreadyExistsInThis } = require('../dist/commons/functions/utils');
+const { getDataInSheetArea, addLinesInSheet, getPlayers, clearPlayersLines } = require('../dist/commons/functions/googlespreadsheet-operations');
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index");
 });
 
 app.post("/", async (req, res) => {
   // const { request, name } = req.body;
+  const { googleSheets, auth } = await prepareCredentials();
+  await clearPlayersLines(googleSheets, auth);
 
+  const getLines = await getDataInSheetArea(googleSheets, auth);
+
+  const playersInSheet = transformTwoDimensionalArrayToListOfPlayers(getLines.data.values);
+  console.log('playersInSheet-----------------------------------');
+  console.log(playersInSheet);
+
+  const playersInFile = getPlayers();
+  console.log('playersInFile------------------------------------');
+  console.log(playersInFile);
+
+  const playerToAdd = [];
+  playersInFile.forEach(player => {
+    if(!valueAlreadyExistsInThis(playersInSheet, 'tag', player.tag )) {
+      const columns = [player.name, player.trophies, player.tag];
+      playerToAdd.push(columns);
+    }
+  });
+
+  console.log('playerToAdd---------------------------------------');
+  console.log(playerToAdd);
+
+  if( !(playerToAdd.length === 0) ){
+    await addLinesInSheet(googleSheets, auth, playerToAdd);
+    res.send("Data has been updated! Thank you!");
+  }
+  else {
+    res.send("Data has been previously updated! Thank you!");
+  }
+});
+
+app.listen(1337, (req, res) => console.log("running on 1337"));
+
+async function prepareCredentials() {
   const auth = new google.auth.GoogleAuth({
     keyFile: "src/commons/files/credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-
-  // Create client instance for auth
   const client = await auth.getClient();
-
-  // Instance of Google Sheets API
   const googleSheets = google.sheets({ version: "v4", auth: client });
-  const spreadsheetId = "10bxlToXdOLsyl2wER2W1TcA9auYmPPqg00RPhIysiZk";
-
-  const players = getPlayers();
-  const lines = [];
-  players.forEach(objeto => {
-    const columns = [objeto.name, objeto.trophies];
-    lines.push(columns);
-  });
-
-  // Write row(s) to spreadsheet
-  await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "Sheet1!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: lines,
-    },
-  });
-
-  res.send("Successfully submitted! Thank you!");
-});
-
-app.listen(1337, (req, res) => console.log("running on 1337"));
+  return { googleSheets, auth };
+}
