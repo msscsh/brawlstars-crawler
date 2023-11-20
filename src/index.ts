61fd621c2742c5bd1310a5f98b840a9f5122c7df
msscsh <itsbruno42@gmail.com>
2023-11-19 22:17:43 -0300

    brand new

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import BrawlStarsAPICaller from "brawlstars-api-sdk";
-
-const client = new BrawlStarsAPICaller({
-	apiKey: "AAAA",
-});
-
-client.getPlayer("#2QPVJ099C").then((player) => {
-	console.log(player);
-});
