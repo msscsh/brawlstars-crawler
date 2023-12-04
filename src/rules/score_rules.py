1e429cf2b7a3f5664950a01686400e7350d15e3d
msscsh <itsbruno42@gmail.com>
2023-12-04 15:49:17 -0300

    adding points to the player score

--- src/rules/score_rules.py
+++ src/rules/score_rules.py
@@ -15,34 +15,43 @@ def apply_general_battlelog_rules_into_players_score(tag, battle):
 	elif  battle["battle"]["type"].lower() == "soloranked" or battle["battle"]["type"].lower() == "teamranked":
 		if battle["battle"]["result"] == "victory":
 			increase_player_battelog_column(tag, 'plWins', 1)
+			increase_player_battelog_column(tag, 'points', 3)
 		if battle["battle"]["result"] == "defeat":
 			increase_player_battelog_column(tag, 'plLosses', 1)
 		if battle["battle"].get("starPlayer") is not None:
 			if battle["battle"].get("starPlayer")['tag'][1:] == tag:
 				increase_player_battelog_column(tag, 'plStarPlayer', 1)
+				increase_player_battelog_column(tag, 'points', 2)
 
 	elif  battle["battle"]["mode"].lower() == "soloshowdown":
 		if battle["battle"]["rank"] == 1:
 			increase_player_battelog_column(tag, 'sdFistPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] == 2:
 			increase_player_battelog_column(tag, 'sdSecondPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] == 3:
 			increase_player_battelog_column(tag, 'sdThirdPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] == 4:
 			increase_player_battelog_column(tag, 'sdFourthPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] >= 5:
 			increase_player_battelog_column(tag, 'sdLosses', 1)
 
 	elif  battle["battle"]["mode"].lower() == "duoshowdown":
 		if battle["battle"]["rank"] == 1:
 			increase_player_battelog_column(tag, 'sdFistPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] == 2:
 			increase_player_battelog_column(tag, 'sdSecondPlace', 1)
+			increase_player_battelog_column(tag, 'points', 1)
 		if battle["battle"]["rank"] >= 3:
 			increase_player_battelog_column(tag, 'sdLosses', 1)
 	else:
 		if battle["battle"]["result"] == "victory":
 			increase_player_battelog_column(tag, 'ngWins', 1)
+			increase_player_battelog_column(tag, 'points', 2)
 		if battle["battle"]["result"] == "defeat":
 			increase_player_battelog_column(tag, 'ngLosses', 1)
 		if battle["battle"]["result"] == "draw":
@@ -52,3 +61,4 @@ def apply_general_battlelog_rules_into_players_score(tag, battle):
 		if battle["battle"].get("starPlayer") is not None:
 			if battle["battle"].get("starPlayer")['tag'][1:] == tag:
 				increase_player_battelog_column(tag, 'starPlayer', 1)
+				increase_player_battelog_column(tag, 'points', 2)
