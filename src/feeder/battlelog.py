bfbcc5476d3c8c41e725debc622b00b064e3224f
msscsh <itsbruno42@gmail.com>
2023-12-12 20:23:53 -0300

    adding reprocess task

--- src/feeder/battlelog.py
+++ src/feeder/battlelog.py
@@ -8,6 +8,18 @@ from util.apis.brawlstars_api import get_api_clubs, get_api_clubs_members, get_a
 from util.db.mongodb import get_db_player_battlelog_data, insert_db_player_battlelog_data, update_db_player_battlelog_data
 from rules.score_rules import apply_general_battlelog_rules_into_players_score
 
+def reprocess_failed(tag):
+    tags_failed_again = []
+    file = 'failed_tag.log'
+    with open(file, 'r') as file_failed_tag:
+        for tag in file_failed_tag:
+            if not main(tag, None) :
+                tags_failed_again = tags_failed_again + [tag.strip()]
+
+    with open(file, 'w') as file_failed_tag:
+        for tag in tags_failed_again:
+            file_failed_tag.write(tag + '\n')
+
 def count_all_player_score_from_battles(tag, battles):
     index = 0
     while index < len(battles):
@@ -32,30 +44,20 @@ def scan_all_players_from_club(club_tag):
     index = 0
     while index < len(members):
         log_line_in_debug(members[index], True)
+        print(f'Player {index} ')
         main(members[index]['tag'][1:], clubBand)
         index += 1
 
-def add_tag_into_crontab_file(tag):
-    file_path = 'cron_tags.log'
-    if not os.path.exists(file_path):
-        with open(file_path, 'w') as novo_arquivo:
-            pass
-    with open(file_path, 'a') as hunted_file:
-        hunted_file.write(f'*/10 * * * * cd $BS_CRAWLER_HOME && python3 src/feeder/battlelog.py {tag}\n')
-
 def main(tag, clubBand):
     log_line(f'Begin with tag: {tag}')
-    # add_tag_into_crontab_file(tag)
-
     api_player_battlelog = get_api_players_battlelog_data_with_name(tag)
-    if clubBand:
-        api_player_battlelog = {'clubBand': clubBand, **api_player_battlelog}
-
     log_line_in_debug(api_player_battlelog, True)
 
     if api_player_battlelog:
-        db_player_battlelog = get_db_player_battlelog_data(tag)
+        if clubBand:
+            api_player_battlelog = {'clubBand': clubBand, **api_player_battlelog}
 
+        db_player_battlelog = get_db_player_battlelog_data(tag)
         if not db_player_battlelog:
             insert_db_player_battlelog_data(tag, api_player_battlelog)
             count_all_player_score_from_battles(tag, api_player_battlelog.get('battles', []))
@@ -65,26 +67,37 @@ def main(tag, clubBand):
             cut = identify_index_last_persisted_change(last_updated_battle_date, api_battles, tag)
             update_db_player_battlelog_data(tag, api_battles[:cut])
             count_all_player_score_from_battles(tag, api_battles[:cut])
+        return True
+
     else:
         log_line(f'Failed getting data from API:{tag}')
+        return False
+
+offset_param = 0
+if 'debug' in sys.argv:
+    offset_param += 1
+
+if 'reprocess_failed' in sys.argv:
+    log_line(f'Init "reprocess_failed"')
+    reprocess_failed(sys.argv[1])
 
 #Club tag
-if len(sys.argv) < 2:
-    print('Use club tag?')
+elif len(sys.argv) < 2+offset_param:
+    log_line('Use club tag?')
     club_tag = input()
     scan_all_players_from_club(club_tag)
 
 #Player tag
-if len(sys.argv) == 2:
+elif len(sys.argv) == 2+offset_param:
     log_line('Loading player tag')
     tag = sys.argv[1]
     main(tag, None)
 
 #Clubs tag
-if len(sys.argv) >= 3:
+elif len(sys.argv) > 3+offset_param:
     log_line('Loading group of clubs')
     index = 1
-    while index < len(sys.argv):
+    while index < len(sys.argv)-offset_param:
         club_tag = sys.argv[index]
         scan_all_players_from_club(club_tag)
         index += 1
