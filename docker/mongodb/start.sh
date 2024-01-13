b9bdef2f25318e129fa33e92b81cd58ee52c4fbe
msscsh <itsbruno42@gmail.com>
2024-01-13 15:52:20 -0300

    giving the script more class

--- docker/mongodb/start.sh
+++ docker/mongodb/start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-if docker volume ls -q -f name=bs_crawler_data | grep -q bs_crawler_data; then
+
+NAME_VOLUME_MONGO_DOCKER=bs_crawler_data
+NAME_CONTAINER_MONGO_DOCKER=mongo_bs_crawler_container
+
+mkdir -p $BS_CRAWLER_HOME/docker/mongodb/data
+
+if docker volume ls -q -f name=$NAME_VOLUME_MONGO_DOCKER | grep -q $NAME_VOLUME_MONGO_DOCKER; then
     echo "Volume already exists."
 else
-	docker volume create -d local -o type=none -o o=bind -o device=$PWD/docker/mongodb/data bs_crawler_data
+	docker volume create -d local -o type=none -o o=bind -o device=$PWD/docker/mongodb/data $NAME_VOLUME_MONGO_DOCKER
     echo "Volume created."
 fi
 
 sh $BS_CRAWLER_HOME/docker/mongodb/stop.sh
 
-docker run -d -p 27017:27017 --name mongo_bs_crawler_container -v bs_crawler_data:/data/db mongo
+docker run -d -p 27017:27017 --name $NAME_CONTAINER_MONGO_DOCKER -v $NAME_VOLUME_MONGO_DOCKER:/data/db mongo
 
